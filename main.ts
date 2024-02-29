3fa001abb2cb1215bcd948af8754e3c4fd4089ef
luiscc07 <149604916+luiscc07@users.noreply.github.com>
2024-02-29 11:00:47 -0600

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,41 @@
-info.onScore(100, function () {
-	
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    PLAYER_ATK = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . 3 3 . . . . . . . . . 
+        . . . 3 3 . . . . . . . . . . . 
+        . . . 3 . . . . . . . . . . . . 
+        . . 3 . 3 . . 3 3 3 3 3 3 3 3 3 
+        . . 3 3 . 3 3 . . . . . . . 3 . 
+        . . 3 3 3 3 3 3 3 . . . . . 3 . 
+        3 3 3 . 3 . . . . 3 3 . . . 3 . 
+        3 . 3 3 . . . . . . . 3 . . 3 . 
+        . 3 3 3 . . . . . . . . 3 . 3 . 
+        . 3 3 . 3 . . . . . . . . 3 3 . 
+        . 3 3 3 3 3 . . . . . . . 3 . . 
+        . . 3 3 3 3 3 3 3 3 3 3 3 3 . . 
+        . . . 3 . . 3 3 3 3 3 3 . . . . 
+        . . . . 3 . . . . . . . . . . . 
+        `, PLYR, 50, 0)
 })
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Projectile, function (sprite, otherSprite) {
+    sprites.destroy(PLAYER_ATK)
+    sprites.destroy(ENMY, effects.fire, 500)
+    wave_number(1, ENMY)
+})
+function wave_number (WAVE: number, ENMY: Sprite) {
+    if (sprites.allOfKind(SpriteKind.Enemy).length > 0) {
+        game.splash("ENEMIES STILL ON MAP ")
+    } else {
+        game.splash("Wave Completed", WAVE)
+        WAVE = WAVE + 1
+        return WAVE
+    }
+    if (WAVE == WAVE_LIST.removeAt(0)) {
+    	
+    }
+    return WAVE
+}
 function EnemySpawn (num: number) {
     for (let index = 0; index < num; index++) {
         ENMY = sprites.create(enemies._pickRandom(), SpriteKind.Enemy)
@@ -8,8 +43,42 @@ function EnemySpawn (num: number) {
     }
 }
 let ENMY: Sprite = null
+let PLAYER_ATK: Sprite = null
 let enemies: Image[] = []
-info.setScore(0)
+let WAVE = 0
+let PLYR: Sprite = null
+let WAVE_LIST: number[] = []
+let CALL_ = [3, 6, 9]
+WAVE_LIST = [5, 10, 15]
+PLYR = sprites.create(img`
+    ........................
+    ........................
+    ........................
+    ........................
+    ..........ffff..........
+    ........ff1111ff........
+    .......fb111111bf.......
+    .......fd1111111f.......
+    ......fdd1111111df......
+    ......fddd111111df......
+    ......fdddddd111df......
+    ......fbddddbfd1df......
+    ......fcbbbdcfddbf......
+    .......fcbb11111f.......
+    ........fffff1b1f.......
+    ........fb111cfbf.......
+    ........ffb1b1ff........
+    ......f.fffbfbf.........
+    ......ffffffff..........
+    .......fffff............
+    ........................
+    ........................
+    ........................
+    ........................
+    `, SpriteKind.Player)
+scene.cameraFollowSprite(PLYR)
+controller.moveSprite(PLYR)
+WAVE = 1
 tiles.setCurrentTilemap(tilemap`level1`)
 enemies = [img`
     . . . . . . . . . . . . . . . . 
@@ -63,4 +132,4 @@ enemies = [img`
     . . c 6 1 1 1 1 1 7 6 6 c c . . 
     . . . c c c c c c c c c c . . . 
     `]
-EnemySpawn(20)
+EnemySpawn(1)
